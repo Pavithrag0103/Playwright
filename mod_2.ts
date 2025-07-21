931e25ef5ef612ea3d8d8d993073b9d90d82fc24
Pavithra.G <pavithrag0103@gmail.com>
2025-07-21 15:26:01 +0530

    21

--- mod_2.ts
+++
@@ -1,9 +0,0 @@
-import{x} from "./modules_15"
-console.log(x);
-
-import {myfun} from "./modules_15"
-myfun()
-
-import {myclass} from "./modules_15"
-var c=new myclass(10,20)
-console.log(c.add());

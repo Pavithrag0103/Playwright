f96fec64d61fc5458703a2b6287cf27188e007f3
Pavithra.G <pavithrag0103@gmail.com>
2025-07-29 14:00:06 +0530

    29

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,18 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
-* Read environment variables from file.
-* https://github.com/motdotla/dotenv
-*/
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
-* See https://playwright.dev/docs/test-configuration.
-*/
-
-
-
+ * See https://playwright.dev/docs/test-configuration.
+ */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -33,26 +30,24 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot:'only-on-failure',
-    video:'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
   projects: [
-    //{
-     // name: 'chromium',
-      //use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    //},
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
-    //{
-     // name: 'firefox',
-     // use: { ...devices['Desktop Firefox'] },
-    //},
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    //{
-     // name: 'webkit',
-      //use: { ...devices['Desktop Safari'] },
-    //},
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -65,9 +60,9 @@ export default defineConfig({
     // },
 
     /* Test against branded browsers. */
-    {
-     name: 'Microsoft Edge',
-     use: { ...devices['Desktop Edge'], channel: 'msedge' },
+     {
+      name: 'Microsoft Edge',
+       use: { ...devices['Desktop Edge'], channel: 'msedge' },
      },
     // {
     //   name: 'Google Chrome',
@@ -82,5 +77,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
- 
